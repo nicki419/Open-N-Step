02c6310622e9ddfec4994b4fc176daa1f0fea5bc
Nick <niklas.braun30419@gmail.com>
2025-05-14 23:12:14 +0200

    Forgot to add pages to commit

---
+++ web/src/Pages/ArduinoPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+interface ArduinoPageProps {
+
+}
+
+const ArduinoPage: React.FC<ArduinoPageProps> = () => {
+  return (
+    <div>
+      <h1>Arduino Page</h1>
+      <p>This is the Arduino page.</p>
+    </div>
+  );
+};
+
+export default ArduinoPage;
---
+++ web/src/Pages/ConfigurationPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+interface ConfigurationPageProps {
+
+}
+
+const ConfigurationPage: React.FC<ConfigurationPageProps> = () => {
+  return (
+    <div>
+      <h1>Configuration Page</h1>
+      <p>This is the Configuration page.</p>
+    </div>
+  );
+};
+
+export default ConfigurationPage;
